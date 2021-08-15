9e2cab8f64e540cf0694932a0c215e711d50f793
lucio Ticali <info@freewebsolution.it>
2021-08-15 10:51:13 +0200

    Naigation

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/users', function () {
+    return view('sections.users.index');
+})->middleware(['auth'])->name('users.index');
+
 require __DIR__.'/auth.php';
