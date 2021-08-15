2d9fd0631bd54a81136b32bc850c25b49a661b17
lucio Ticali <info@freewebsolution.it>
2021-08-15 17:33:12 +0200

    Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Access\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->register();
+
+        Gate::define('admin',function($user){
+            return $user->isAdmin;
+        });
+
+        Gate::define('guest',function($user){
+            return !$user->isAdmin;
+        });
     }
 }
