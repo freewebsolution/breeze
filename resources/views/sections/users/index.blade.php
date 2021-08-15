afe70a8e689ac8aea122c5f578a4c07cd5e222df
lucio Ticali <info@freewebsolution.it>
2021-08-15 11:54:32 +0200

    Correggi le icone

--- resources/views/sections/users/index.blade.php
+++ resources/views/sections/users/index.blade.php
@@ -67,11 +67,8 @@ border-b border-gray-200 sm:table-cell">
 text-right border-b border-gray-200 md:px-6 md:whitespace-no-wrap">
                                     <div class="flex justify-end space-x-1">
                                         <a href="{{route('users.edit',$user->id)}}" class="p-1 ml-3 border-2 border-indigo-200 rounded-md hover:bgindigo-100">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                 stroke="currentColor" class="w-4 h-4 text-indigo-500">
-                                                <path stroke-linecap="round"
-                                                      stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z">
-                                                </path>
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-indigo-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                             </svg>
                                         </a>
                                         <form action="{{route('users.destroy',
@@ -82,13 +79,9 @@ $user->id)}}" method="POST" class="">
  <button class="p-1 border-2
 border-red-200 rounded-md hover:bg-red-100">
 
- <svg
-     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-     stroke="currentColor" class="w-4 h-4 text-red-500">
- <path strokelinecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867
-12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-
-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
- </svg>
+<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+</svg>
  </button>
  </span>
                                         </form>
